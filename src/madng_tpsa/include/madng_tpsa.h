40759f5311de552649d6480f20ea3d0512ed7294
Szymon Łopaciuk <szymon@lopaciuk.eu>
2026-08-25 09:48:33 +0200

    Add possibility for C API implementers to set an error callback

--- src/madng_tpsa/include/madng_tpsa.h
+++ src/madng_tpsa/include/madng_tpsa.h
@@ -1,8 +1,22 @@
 #ifndef MADNG_TPSA_H
 #define MADNG_TPSA_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "mad_tpsa.h"
 
+/*
+ * Install a thread-local callback for fatal MAD-NG errors and return the
+ * previously installed callback. The callback must not return.
+ */
+typedef void (*madng_tpsa_error_handler)(const char *location, const char *message);
+madng_tpsa_error_handler madng_tpsa_set_error_handler(madng_tpsa_error_handler handler);
+
+/*
+ * Helper functions for this library.
+ */
 int madng_tpsa_check_tpsa_compatibility(const tpsa_t *left, const tpsa_t *right);
 int madng_tpsa_tpsa_variable_index(const tpsa_t *series);
 int madng_tpsa_tpsa_single_monomial(
@@ -10,5 +24,9 @@ int madng_tpsa_tpsa_single_monomial(
     int monomial_length,
     ord_t monomial_orders[]
 );
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // MADNG_TPSA_H
